7774cddbbb7ede6629003d20964ba105d2a77a8c
vendmachs8 <vendmach.studio8@gmail.com>
2025-03-29 15:30:13 +0700

    testttt

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // Base URL harus sesuai dengan root Vercel
-  build: {
-    outDir: '.', // Output ke root directory (sesuai vercel.json)
-    emptyOutDir: true, // Kosongkan directory build sebelumnya
-    rollupOptions: {
-      input: '/index.html', // Sesuai dengan src di vercel.json
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
-  },
   server: {
-    historyApiFallback: true, // Penting untuk SPA routing
-    strictPort: true,
-    port: 5001 // Sesuaikan dengan port development Anda
+    host: true, // Ini akan memungkinkan akses dari jaringan
+    port: 5001, // Port yang digunakan
   },
-  optimizeDeps: {
-    include: ['vue'] // Optimasi dependensi Vue
-  }
-})
+});
+
