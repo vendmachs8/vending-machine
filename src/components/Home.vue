f080faf225766182f67491265a95e78654d67d3e
vendmachs8 <vendmach.studio8@gmail.com>
2025-04-03 15:36:02 +0700

    4/3/2025 v.3

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -546,7 +546,7 @@ export default {
 
     const API_BASE_URL = ref(
       process.env.NODE_ENV === 'development' 
-        ? 'http://localhost:3000' 
+        ? 'https://d750-114-10-46-80.ngrok-free.app' 
         : 'https://vendmach-vendmachs8s-projects.vercel.app' // Gunakan domain Vercel di production
     );
 
